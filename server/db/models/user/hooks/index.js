ffd671f8fa1b2fe78c753f7bd46fa1a2d83e9f83
lucas1richard <lucas1richard@gmail.com>
2018-10-26 16:11:53 -0400

    more secure password storage, force db sync

--- server/db/models/user/hooks/index.js
+++ server/db/models/user/hooks/index.js
@@ -1,29 +1,34 @@
 const crypto = require('crypto');
 
-var genRandomString = function(length){
+const genRandomString = function(length){
   return crypto.randomBytes(Math.ceil(length / 2))
     .toString('hex') /** convert to hexadecimal format */
     .slice(0,length);   /** return required number of characters */
 };
 
+const generatePassSalt = (password) => {
+  const salt = genRandomString(16);
+  const hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  const value = hash.digest('hex');
+  return {
+    password: value,
+    salt
+  };
+};
+
 const hooks = {
   /* eslint-disable no-param-reassign */
   beforeCreate(user) {
-    const salt = genRandomString(16);
-    const hash = crypto.createHmac('sha512', salt);
-    hash.update(user.password);
-    const value = hash.digest('hex');
-    user.password = value;
+    const { password, salt } = generatePassSalt(user.password);
+    user.password = password;
     user.salt = salt;
     return user;
   },
   beforeBulkCreate(users) {
     users = users.map((user) => {
-      const salt = genRandomString(16);
-      const hash = crypto.createHmac('sha512', salt);
-      hash.update(user.password);
-      const value = hash.digest('hex');
-      user.password = value;
+      const { password, salt } = generatePassSalt(user.password);
+      user.password = password;
       user.salt = salt;
       return user;
     });
