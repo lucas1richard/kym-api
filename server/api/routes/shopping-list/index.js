703942e489ee3bc3d302519e8d5125fef741a0b5
Richard <lucas1richard@gmail.com>
2022-09-20 20:40:20 -0400

    feat(versioned-routes): /shopping-list route

--- server/api/routes/shopping-list/index.js
+++ server/api/routes/shopping-list/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const getList = require('./controllers/getList');
+const getListV1 = require('./controllers/getListV1');
 
 module.exports = router;
 
-router.get('/list', getList);
+router.get('/list/v1', async (req, res, next) => {
+  const { date, numdays } = req.query;
+  const { uuid } = res.locals;
+  const records = await getListV1({ date, numDays: numdays, uuid });
+  res.json(records);
+});
